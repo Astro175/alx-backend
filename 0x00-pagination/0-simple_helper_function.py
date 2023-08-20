76341bd090ce3905ffaab2ad091eba1843a3eb29
astro175 <aspark400@gmail.com>
2023-08-20 11:05:15 +0100

    fixed annotations

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-
+from typing import Tuple
 """
 The module should return a tuple of size two containing a start index
 and an end index corresponding to the range of indexes to return in a
 list for those particular pagination parameters
 """
 
-def index_range(page: int, page_size: int) -> tuple[int]:
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
     This function returns start_index and end_index for a range
     in a particular pagination parameters
