02c0eec639a5eea5223bbef5d4b5458a684aab92
astro175 <aspark400@gmail.com>
2023-09-16 17:40:20 +0100

    fixed pep8

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -8,6 +8,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     """
@@ -16,5 +17,5 @@ def index():
     return render_template('0-index.html')
 
 
-if __name__ == '__main__': 
-   app.run()
+if __name__ == '__main__':
+    app.run()
--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -5,17 +5,21 @@
   Babel translator
 """
 
-from flask import Flask
+from flask import Flask, request
 from flask_babel import Babel
 
 app = Flask(__name__)
 babel = Babel(app)
+
 
 class Config(object):
     """
       Babel config class
     """
     LANGUAGES = ["en", "fr"]
+
+
 @babel.localeselector
 def get_locale():
+    """Gets local and translate"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
