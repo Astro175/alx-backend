571741fee0fbad1e90ed9ae335bff47321d7525f
astro175 <aspark400@gmail.com>
2023-09-17 10:44:28 +0100

    added solution to task 2

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+"""
+  Module that instantiates a flask and
+  Babel translator
+"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+
+class Config(object):
+    """
+      Babel config class
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """Chooses the best match for supported languages"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    """Index page"""
+    return render_template('2-index.html')
+
+
+if __name__ == '___main__':
+    app.run()
